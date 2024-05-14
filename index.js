c1288fead32b8b2074180397b6db337ff5131f46
Diesos <ozturk@live.Fr>
2024-05-14 21:14:50 +0200

    refactor: Update Firebase integration to use onValue instead of push for real-time updates

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+//Database connection
 const appSettings = {
     databaseURL: "https://playground-b00cd-default-rtdb.europe-west1.firebasedatabase.app/"
 }
-
+// Database  initializer
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const foodsInDb = ref(database, "foods")
+const foodsInDb = ref(database, "Foods")
 
-console.log(app)
-console.log(database)
-
+// Elem selector
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingUl = document.getElementById("shopping-list")
 
+onValue(foodsInDb, function(snapshot) {
+    let foodsArray = Object.values(snapshot.val())
+    clearList()
+    for (let i = 0; i < foodsArray.length; i++){
 
+        let currentFood = foodsArray[i]
+        addElementToList(currentFood)
+        clearInputEl(inputFieldEl)
+    }
 
+})
+
+// Function to add element to the list
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     if (inputValue !== ""){
-        shoppingUl.innerHTML += `<li>${inputValue}</li>`
+        addElementToList(inputValue)
         push(foodsInDb, inputValue)
 
         console.log(foodsInDb, inputValue)
-        inputFieldEl.value = ""
+        clearInputEl(inputFieldEl)
     }
 })
+// Function to clear the input field
+function clearInputEl(elem){
+    elem.value = ""
+}
+//Function to add the value of input field as an <li> innerhtml.
+function addElementToList(elem){
+    shoppingUl.innerHTML += `<li>${elem}</li>`
+}
+
+function clearList(){
+    shoppingUl.innerHTML = ""
+}
